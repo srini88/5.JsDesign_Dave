254e00b232d883de845e9eabe09aacbf64bad98c
srini88 <pvsrinivas.ece@gmail.com>
2016-04-29 16:26:53 -0500

    closure awesome expample

--- js.js
+++ js.js
@@ -2,17 +2,30 @@ window.onload = function(){
 
 
 	var output = document.getElementById("output");
-	output.innerHTML = myNonClosure();
+
+	var closure = myClosure();  ///is still in scope..
+
+	output.innerHTML = closure();
+
+
 
 
 	window.setTimeout(function(){
-			output.innerHTML += '<br/>' + myNonClosure();
-		}, 500);   //after 500 sec...creating new date object on every call..to prove
-
+			output.innerHTML += '<br/>' + closure();
+		}, 500);   //after 500 sec...we get the same ms written out
 
 
 	function myNonClosure(){
 		var date = new Date();
 		return date.getMilliseconds();
+	}
+
+
+	//classs - 
+	function myClosure(){  //class def
+		var date = new Date();  //closure .. date object hangs around//as long as myClosure() is in scope
+		return function(){
+			return date.getMilliseconds();
+		};
 	}
 }
