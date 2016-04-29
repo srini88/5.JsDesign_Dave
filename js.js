db1e8fcd5b37fb06d92f3ae0a7c1264a16e828d3
srini88 <pvsrinivas.ece@gmail.com>
2016-04-29 16:17:11 -0500

    closure starting

--- js.js
+++ js.js
@@ -1,10 +1,14 @@
-function myNonClosure (){
-
-	var date = new Date();
-	return date.getMilliseconds();
-}  //hwhen we return date.getmil = this date variable is gone..
-
-console.log(myNonClosure());   //this shit is not a closre
 
 
-//we want the date variable to even stick around, we got to use closure...
+function myClosure(){
+
+	var date = new Date();  //this date variable of the object sticks around..
+	//nested function....this date sticks around..
+	return function(){
+		return date.getMilliseconds();
+	}
+} ///this closure allows us to create a container for our variables and our functions..
+
+console.log(myClosure());  //you get a function here..
+
+console.log(myClosure()());  //you get a value here...
