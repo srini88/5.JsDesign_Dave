07c4e076d939dae374df38ffd5eec1d8bd4d2736
srini88 <pvsrinivas.ece@gmail.com>
2016-05-21 20:50:10 -0500

    b not global

--- js.js
+++ js.js
@@ -1,19 +1,20 @@
 
 function srini(){
+	// var foo = {n: 1};
+	// var bar = foo;  //bar and foo point to the same thing..
+	// foo.x = foo = {n: 2};
+	// console.log(foo);
 
-var a = b =5;
+	var b=2;
+
+	var a=b=5;
 
 
-console.log(a);
 }
 
-// It will be global b=5;
-
-// var a=b;
 srini();
+console.log(b);  //b is not defined...b wont be global this time...since var b is there already...apply this thing to foo
 
-//console.log(a);  //a is not defined
-console.log(b);  //this is global whattttttttt  prints 5
 
 
 
