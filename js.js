aa49c642f6aa7332384f8e4dad6f9dc68dacd230
srini88 <pvsrinivas.ece@gmail.com>
2016-05-21 20:59:19 -0500

    foo bar

--- js.js
+++ js.js
@@ -1,19 +1,28 @@
 
 function srini(){
-	// var foo = {n: 1};
-	// var bar = foo;  //bar and foo point to the same thing..
-	// foo.x = foo = {n: 2};
-	// console.log(foo);
+	var foo = {n: 1};
+	var bar = foo;  //bar and foo point to the same thing..
+	foo.x = foo = {n: 2};  //foo={n:2} destroying old address..makes it completely new address
+	
 
-	var b=2;
+	//foo.x = foo;   //
+	console.log("here" , foo.x);
+	//foo ={n : 2};   //but here it destroys and completely creates a new object..
 
-	var a=b=5;
+	console.log("after", foo);  //only  {n:2}  and no foo.x...
+
+	console.log(foo.x);   //undefined...
+	console.log("bar",bar);
+
+	console.log(bar.x)   //object {n:2}
+	console.log(foo);
+
 
 
 }
 
 srini();
-console.log(b);  //b is not defined...b wont be global this time...since var b is there already...apply this thing to foo
+
 
 
 
